869faa63d2528be6e5ae4f563ae87581e86320d4
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2025-11-30 13:21:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from postgrest.exceptions import APIError   # ✅ catch Supabase DB errors
 
 app = Flask(__name__)
 app.secret_key = "super-secret-key"
-ADMIN_PASSWORD = "ugitsplacement"
+ADMIN_PASSWORD = "itsplacement25"
 
 # ---------------- Supabase Setup ----------------
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
